52ce2cc4b900c9842802b0e47e0361328ffc157b
1313ou <1313ou@gmail.com>
2024-11-02 21:11:02 +0100

    bew version 2.2.2

--- grind_offsets.sh
+++ grind_offsets.sh
@@ -17,5 +17,5 @@ fi
 mkdir -p "${OUTDIR}"
 echo "DIR:   "${OUTDIR}"" 1>&2;
 
-jar=target/yaml2wndb-2.2.1-uber.jar
+jar=target/yaml2wndb-2.2.2-uber.jar
 java -ea -cp "${jar}" org.oewntk.grind.yaml2wndb.GrindOffsets "${IN}" "${OUTDIR}"
