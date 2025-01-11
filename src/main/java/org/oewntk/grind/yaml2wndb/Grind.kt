51aad58165eba006d41cefeff6a0dbc98292d9a5
1313ou <1313ou@gmail.com>
2025-01-11 20:07:55 +0100

    fixes bug in wndb example output

--- src/main/java/org/oewntk/grind/yaml2wndb/Grind.kt
+++ src/main/java/org/oewntk/grind/yaml2wndb/Grind.kt
@@ -88,7 +88,7 @@ object Grind {
         // Supply model
         Tracing.progress("before model is supplied,", startTime)
         val model = Factory(inDir, inDir2).get()
-        //Tracing.psInfo.printf("[Model] %s%n%s%n%n", Arrays.toString(model.getSources()), model.info());
+        //Tracing.psInfo.printf("[Model] %s%n%s%n%n", Arrays.toString(model.getSources()), model.info())
         Tracing.progress("after model is supplied,", startTime)
 
         // Consume model
